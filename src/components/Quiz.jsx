ac50fe4794287d0f89f819ffc69b0ac79e81e7b3
Matt Frutchey <mjfru201@gmail.com>
2025-05-25 15:23:21 -0400

    begin filling out the skeleton of the quiz component, add radio buttons, labels, comment out future code that will render errors at the moment

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,41 @@
+import { useParams } from "react-router-dom";
+import { russianCyrillic } from "../data/ru_cyrillic";
+import { ukrainianCyrillic } from "../data/uk_cyrillic";
 import ActivityNav from "./ActivityNav";
+// import { useState } from "react";
+
+const datasets = {
+	russian: russianCyrillic,
+	ukrainian: ukrainianCyrillic,
+};
 
 const Quiz = () => {
+	// const { userAnswer, userUserAnswer } = useState("");
 
+	// const { language } = useParams();
+	// const selectedLanguage = datasets[language];
+
+	// const quizFive = [];
+	// const quizTen = [];
+	// const quizAll = [];
 
 	return (
 		<main>
 			<h1 className="quiz-test">This is the future home of quizzes.</h1>
 			<ActivityNav />
-      <div className="quiz-container">
-        <p>Quiz Placeholder</p>
-        <input type="text" />
-        <button>Submit</button>
-      </div>
+			<div className="quiz-container">
+				<p>Quiz Placeholder</p>
+				<div className="quiz-select">
+					<label htmlFor="">Quiz All Letters:</label>
+					<input type="radio" name="quizSelect" id="" defaultChecked/>
+					<label htmlFor="">Quiz 10 Random Letters:</label>
+					<input type="radio" name="quizSelect" id="" />
+					<label htmlFor="">Quiz 5 Random Letters:</label>
+					<input type="radio" name="quizSelect" id="" />
+				</div>
+				<input type="text" />
+				<button>Submit</button>
+			</div>
 		</main>
 	);
 };
