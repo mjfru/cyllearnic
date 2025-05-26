3b2bf563813cff68eb2460be3596ed8aabd25195
Matt Frutchey <mjfru201@gmail.com>
2025-05-26 15:35:46 -0400

    set up state for user answers in the quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,18 @@
-import { useParams } from "react-router-dom";
-import { russianCyrillic } from "../data/ru_cyrillic";
-import { ukrainianCyrillic } from "../data/uk_cyrillic";
+// import { useParams } from "react-router-dom";
+// import { russianCyrillic } from "../data/ru_cyrillic";
+// import { ukrainianCyrillic } from "../data/uk_cyrillic";
 import ActivityNav from "./ActivityNav";
-// import { useState } from "react";
+import { useState } from "react";
 
-const datasets = {
-	russian: russianCyrillic,
-	ukrainian: ukrainianCyrillic,
-};
+// const datasets = {
+// 	russian: russianCyrillic,
+// 	ukrainian: ukrainianCyrillic,
+// };
 
 const Quiz = () => {
-	// const { userAnswer, userUserAnswer } = useState("");
+	const [ userAnswer, setUserAnswer ] = useState("");
+  console.log(userAnswer);
+  
 
 	// const { language } = useParams();
 	// const selectedLanguage = datasets[language];
@@ -27,13 +29,17 @@ const Quiz = () => {
 				<p>Quiz Placeholder</p>
 				<div className="quiz-select">
 					<label htmlFor="">Quiz All Letters:</label>
-					<input type="radio" name="quizSelect" id="" defaultChecked/>
+					<input type="radio" name="quizSelect" id="" defaultChecked />
 					<label htmlFor="">Quiz 10 Random Letters:</label>
 					<input type="radio" name="quizSelect" id="" />
 					<label htmlFor="">Quiz 5 Random Letters:</label>
 					<input type="radio" name="quizSelect" id="" />
 				</div>
-				<input type="text" />
+				<input
+					type="text"
+					value={userAnswer}
+					onChange={(e) => setUserAnswer(e.target.value)}
+				/>
 				<button>Submit</button>
 			</div>
 		</main>
