03653da0435fce55294fb058ef16e609216f169a
Matt Frutchey <mjfru201@gmail.com>
2025-06-03 15:16:50 -0400

    rearrange header, import a font for eventual print/cursive toggle, utilize useNavigate for dropdown navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		const selectedLanguage = e.target.value;
+		if (selectedLanguage) {
+			navigate(`/study/${selectedLanguage}`);
+		}
+	};
+
 	return (
-		<div className="nav-header">
+		<header className="header">
 			<Link to="/">
 				<h3 className="logo">Cyllearnic</h3>
 			</Link>
 
 			<nav className="nav-container">
-				<ul>
-					<li>Home</li>
-					<li>About</li>
-					<li>Select Language</li>
-				</ul>
+				<Link to="/">Home</Link>
+				<Link to="">About</Link>
+				<div className="language-nav">
+					<p>Switch Language: </p>
+					<select name="" id="" onChange={handleChange}>
+						<option value="" disabled>
+							{/* Why is this disappearing after initial render? -- Maybe move this into a component by itself */}
+							Choose a language
+						</option>
+						<option value="russian">Russian</option>
+						<option value="ukrainian">Ukrainian</option>
+					</select>
+				</div>
 			</nav>
-		</div>
+		</header>
 	);
 };
 
