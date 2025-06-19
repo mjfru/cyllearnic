ae43ddaf86e49acb92196732f2ef9b54580f211b
Matt Frutchey <mjfru201@gmail.com>
2025-06-19 13:10:29 -0400

    replace placeholder activity buttons with bootstrap, shrink main activity pages, readjust grids

--- src/components/ActivityNav.jsx
+++ src/components/ActivityNav.jsx
@@ -9,13 +9,13 @@ const ActivityNav = () => {
 		<>
 			<div className="activity-nav">
 				<Link to={`/study/${language}`}>
-					<button className="activity-link">Study</button>
+					<button type="button" className="btn btn-primary activity-link">Study</button>
 				</Link>
 				<Link to={`/flashcards/${language}`}>
-					<button className="activity-link">Flashcards</button>
+					<button type="button" className="btn btn-primary activity-link">Flashcards</button>
 				</Link>
 				<Link to={`/quiz/${language}`}>
-					<button className="activity-link">Quiz</button>
+					<button type="button" className="btn btn-primary activity-link">Quiz</button>
 				</Link>
 			</div>
 			<div className="toggle-container">
