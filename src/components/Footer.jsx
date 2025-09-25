95181512b4fb64193c6ef8ce33c8afa880b5d352
Matt Frutchey <mjfru201@gmail.com>
2025-09-25 10:38:36 -0400

    add project development warning to footer to prepare for deployment on vercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ const Footer = () => {
 
 	return (
 		<footer className="footer-container">
+			<p style={{ color: "red", fontWeight: "bold", textAlign: "center", fontSize: "16px", marginBottom: "-2px" }}>
+				This app is in active development and is approximately 60% finished. Many features, styles, and
+				functionality will be added, changed or removed. Currently compatible
+				with larger screen sizes only.
+			</p>
 			<h3>Hand-coded and Maintained by MJF - {currentYear}</h3>
 			<div className="personal-link-container">
 				<a href="https://github.com/mjfru" target="_blank">
@@ -12,12 +17,12 @@ const Footer = () => {
 					Developer Portfolio
 				</a>
 			</div>
-				<p>
-					This project is focused on helping users learn and appreciate the
-					diversity of Cyrillic alphabets and the cultures that use them. All
-					countries, languages, and cultures are approached with respect and
-					neutrality.
-				</p>
+			<p>
+				This project is focused on helping users learn and appreciate the
+				diversity of Cyrillic alphabets and the cultures that use them. All
+				countries, languages, and cultures are approached with respect and
+				neutrality.
+			</p>
 		</footer>
 	);
 };
