a5e7a1324ee666dfb7adf42a832d723ac87a7c3d
Matt Frutchey <mjfru201@gmail.com>
2026-08-18 08:26:12 -0400

    continue working on mn cyrillic structure

--- src/data/mn_cyrllic.js
+++ src/data/mn_cyrllic.js
@@ -1,1 +1,10 @@
-export const mongolianCyrillic = [];
+export const mongolianCyrillic = [
+	{
+		id: "mn01",
+		letterUpper: "А",
+		letterLower: "а",
+		pronunciation: "A",
+		example: "Ажил",
+		meaning: "Work",
+	},
+];
