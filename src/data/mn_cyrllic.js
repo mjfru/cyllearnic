94e5371ab1c67cc40b28abb1aee7ada7e468f8d4
Matt Frutchey <mjfru201@gmail.com>
2026-08-20 00:00:19 -0400

    more work done to mg cyrillic dataset

--- src/data/mn_cyrllic.js
+++ src/data/mn_cyrllic.js
@@ -7,12 +7,28 @@ export const mongolianCyrillic = [
 		example: "Ажил",
 		meaning: "Work",
 	},
-  {
+	{
 		id: "mn02",
 		letterUpper: "Б",
 		letterLower: "б",
 		pronunciation: "B",
 		example: "Баяр",
 		meaning: "Joy / celebration",
+	},
+	{
+		id: "mn03",
+		letterUpper: "В",
+		letterLower: "в",
+		pronunciation: "V",
+		example: "Вагон",
+		meaning: "Train car",
+	},
+	{
+		id: "mn04",
+		letterUpper: "Г",
+		letterLower: "г",
+		pronunciation: "G",
+		example: "Гэр",
+		meaning: "Ger (traditional dwelling)",
 	},
 ];
